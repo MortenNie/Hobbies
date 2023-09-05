c62c048e74184f52b44bc708519cdf714aa9dd9a
MortenNie <cph-mn705@cphbusiness.dk>
2023-09-05 10:05:23 +0200

    commit

--- src/main/java/dat/HobbyDAO.java
+++ src/main/java/dat/HobbyDAO.java
@@ -1,8 +1,10 @@
 package dat;
 
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Query;
 import jakarta.persistence.TypedQuery;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class HobbyDAO {
@@ -21,7 +23,41 @@ public class HobbyDAO {
 
         }
     }
+
+    public Integer retrieveAllInterests(Integer hobbyId) {
+
+
+        try (var em = emf.createEntityManager()) {
+
+            Query query = em.createQuery("SELECT count(hp) FROM HobbyPerson hp WHERE hp.hobby.id = :parameter");
+            query.setParameter("parameter", hobbyId);
+
+            return (Integer) query.getSingleResult();
+
+
+        }
+    }
+
+        public HashMap<String, Integer> retrieveAllHobbiesPlusInterest() {
+            HashMap<String, Integer> hobbiesPlusInterest = new HashMap<>();
+            HobbyDAO dao = new HobbyDAO();
+            List<Hobby> hobbies = dao.retrieveAllHobby();
+
+            for (Hobby s : hobbies) {
+
+                Integer interestNumbers = retrieveAllInterests(s.getId());
+                hobbiesPlusInterest.put(s.getName(),interestNumbers);
+
+            }
+           return hobbiesPlusInterest;
+        }
+
+
 }
+
+
+
+
 
 
 
--- src/main/java/dat/Main.java
+++ src/main/java/dat/Main.java
@@ -3,11 +3,12 @@ package dat;
 import jakarta.persistence.EntityManagerFactory;
 
 import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Person gudrund = new Person("Gudrund", "Jensen", LocalDate.of(2023, 9, 4));
+    /*    Person gudrund = new Person("Gudrund", "Jensen", LocalDate.of(2023, 9, 4));
         PersonDetails gudrundsPersonalDetails = new PersonDetails("New Kagevej 7", "Kagerup", 8000);
         gudrund.addPersonalDetails(gudrundsPersonalDetails);
         Hobby hobby = new Hobby("Kage-making", "General", Hobby.Type.Inside);
@@ -22,8 +23,10 @@ public class Main {
             em.persist(gudrund);
             em.getTransaction().commit();
 
-        }
-
+        } */
+        HobbyDAO dao = new HobbyDAO();
+        HashMap<String, Integer> interestplushobbies = dao.retrieveAllHobbiesPlusInterest();
+        System.out.println(interestplushobbies);
 
     }
 }
