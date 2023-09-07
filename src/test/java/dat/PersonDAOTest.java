8c13d183d82cd5beca8f6a7b7d69665cddffe660
LUJHNA <Jakob_h_95@hotmail.com>
2023-09-07 11:31:42 +0200

    TestsAdded
    TestsAdded

---
+++ src/test/java/dat/PersonDAOTest.java
@@ -0,0 +1,20 @@
+package dat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonDAOTest {
+
+    @Test
+    void getPersonInfo() {
+    }
+
+    @Test
+    void getPersonByTelephoneNumber() {
+    }
+
+    @Test
+    void getPersonByNumber() {
+    }
+}
---
+++ src/test/java/dat/PersonDetailsDAOTest.java
@@ -0,0 +1,28 @@
+package dat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonDetailsDAOTest {
+
+    @Test
+    void getAllPersonDetails() {
+    }
+
+    @Test
+    void getAllContactInfo() {
+    }
+
+    @Test
+    void getAllInfoFromID() {
+    }
+
+    @Test
+    void retrieveAllPersonsByCity() {
+    }
+
+    @Test
+    void retrieveAllCitiesAndZips() {
+    }
+}
